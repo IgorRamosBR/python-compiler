3873c941ccdc0282667f854b36e7ff657684ceef
IgorRamosBR <igorir7@gmail.com>
2018-05-23 02:47:06 -0300

    refactoring parser

--- parse.py
+++ parse.py
@@ -1,6 +1,6 @@
 class Parser(object):
-    toks = ""
-    symbols = {}
+    toks = "" #tokens
+    symbols = {} #tabela de simbolos
 
     def __init__(self, tokens):
         self.toks = tokens
@@ -9,10 +9,7 @@ class Parser(object):
         if (self.toks[i] + " " + self.toks[i+1][0:6] == "PRINT STRING") or self.toks[i] + " " + self.toks[i+1][0:3] == "PRINT NUM" or self.toks[i] + " " + self.toks[i+1][0:4] == "PRINT EXPR" or self.toks[i] + " " + self.toks[i+1][0:3] == "PRINT VAR":
             return True
 
-    def evalExpression(self, expr):
-            return eval(expr)
-
-    def getVARIABLE(self, varname):
+    def buscaVariavel(self, varname):
         varname = varname[4:]
         if varname in self.symbols:
             return self.symbols[varname]
@@ -20,11 +17,29 @@ class Parser(object):
             return "VARIABLE ERRROR: Undefined Variable"
             exit()
 
+    def atribui(self, i):
+        if self.toks[i+2][0:6] == "STRING":
+            self.symbols[self.toks[i][4:]] = self.toks[i+2]
+        elif self.toks[i+2][0:3] == "NUM":
+            self.symbols[self.toks[i][4:]] = self.toks[i+2]
+        elif self.toks[i+2][0:4] == "EXPR":
+            self.symbols[self.toks[i][4:]] = "NUM:" + str(eval(self.toks[i+2][5:]))
+        elif self.toks[i+2][0:3] == "VAR":
+            self.symbols[self.toks[i][4:]] = self.buscaVariavel(self.toks[i+2])
+
+    def fazAtribuicao(self, i):
+        if self.toks[i][0:3] + " " + self.toks[i+1] + " " + self.toks[i+2][0:6] == "VAR EQUALS STRING" or self.toks[i][0:3] + " " + self.toks[i+1] + " " + self.toks[i+2][0:3] == "VAR EQUALS NUM" or self.toks[i][0:3] + " " + self.toks[i+1] + " " + self.toks[i+2][0:4] == "VAR EQUALS EXPR" or self.toks[i][0:3] + " " + self.toks[i+1] + " " + self.toks[i+2][0:3] == "VAR EQUALS VAR":
+            self.atribui(i)
+            return True
+
+    def evalExpression(self, expr):
+            return eval(expr)
+
     def doPrint(self, toPRINT):
         if(toPRINT[0:6] == "STRING"):
             toPRINT = toPRINT[8:]
             toPRINT = toPRINT[:-1]
-            print(toPRINT) #diferente
+            print(toPRINT) 
         elif(toPRINT[0:3] == "NUM"):
             toPRINT = toPRINT[4:]
             print(toPRINT)
@@ -32,15 +47,21 @@ class Parser(object):
             toPRINT = self.evalExpression(toPRINT[5:])
             print(toPRINT)
         elif toPRINT[0:3] == "VAR":
-            self.doPrint((self.getVARIABLE(toPRINT)))
-        
+            self.doPrint((self.buscaVariavel(toPRINT)))
 
-    def doASSIGN(self, varname, varvalue):
-        self.symbols[varname[4:]] = varvalue
+    def realizaInput(self, i):
+        if self.toks[i] + " " + self.toks[i+1][0:6] + " " + self.toks[i+2][0:3] == "INPUT STRING VAR":
+            string = self.toks[i+1][7:]
+            varname = self.toks[i+2][4:]
 
-    def getINPUT(self, string, varname):
-        i = input(string[1:-1] + " ")
-        self.symbols[varname] = "STRING:\"" + i + "\""
+            i = input(string[1:-1] + " ")
+            self.symbols[varname] = "STRING:\"" + i + "\""
+
+            return True
+
+    def verificaIf(self, i):
+        if self.toks[i] + " " + self.toks[i+1][0:3] + " " + self.toks[i+2] + " " + self.toks[i+3][0:3] + " " + self.toks[i+4] == "IF NUM EQEQ NUM THEN":
+            return True
 
     def parse(self):
         i = 0
@@ -50,25 +71,11 @@ class Parser(object):
             elif(self.verificaPrint(i)):
                 self.doPrint(self.toks[i+1])
                 i += 2
-            elif self.toks[i][0:3] + " " + self.toks[i+1] + " " + self.toks[i+2][0:6] == "VAR EQUALS STRING"  or self.toks[i][0:3] + " " + self.toks[i+1] + " " + self.toks[i+2][0:3] == "VAR EQUALS NUM" or self.toks[i][0:3] + " " + self.toks[i+1] + " " + self.toks[i+2][0:4] == "VAR EQUALS EXPR" or self.toks[i][0:3] + " " + self.toks[i+1] + " " + self.toks[i+2][0:3] == "VAR EQUALS VAR":
-                if self.toks[i+2][0:6] == "STRING":
-                    self.doASSIGN(self.toks[i],self.toks[i+2])
-                elif self.toks[i+2][0:3] == "NUM":
-                    self.doASSIGN(self.toks[i],self.toks[i+2])
-                elif self.toks[i+2][0:4] == "EXPR":
-                    self.doASSIGN(self.toks[i],"NUM:" + str(eval(self.toks[i+2][5:])))
-                elif self.toks[i+2][0:3] == "VAR":
-                    self.doASSIGN(self.toks[i],self.getVARIABLE(self.toks[i+2]))
+            elif (self.fazAtribuicao(i)):
                 i+=3
-            elif self.toks[i] + " " + self.toks[i+1][0:6] + " " + self.toks[i+2][0:3] == "INPUT STRING VAR":
-                self.getINPUT(self.toks[i+1][7:], self.toks[i+2][4:])
+            elif (self.realizaInput(i)):
                 i+=3
-            elif self.toks[i] + " " + self.toks[i+1][0:3] + " " + self.toks[i+2] + " " + self.toks[i+3][0:3] + " " + self.toks[i+4] == "IF NUM EQEQ NUM THEN":
-      
-               # if self.toks[i+1][4:] == self.toks[i+3][4:]:
-               #     print("True")
-               # else:
-               #     print("false")
+            elif (self.verificaIf(i)):
                 i+=5
 
 
